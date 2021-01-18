8d3119eb2b3495db330483b9cc77631d54a29305
Stanislas LEROY <stanislas.leroy@enfrasys.fr>
2021-01-18 16:50:27 +0100

    Initial import

---
+++ dmp-back-end/src/test/java/fr/stanislasleroy/DMP/DmpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.stanislasleroy.DMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
